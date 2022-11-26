f65fa6c80e9620d5e7cf9a9519219c5c1f856410
Tatyanata <115730789+Tatyanata@users.noreply.github.com>
2022-11-27 05:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ from extensions import APIException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
-def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    text = 'Привет! Я Бот-конвертер. Я могу: \n- Произвести конвертацию через запрос формат: <имя валюты> \
 <в какую валюту перевести> \
-<количество перводимой валюты>\nЧтобы увидеть список доступных валют введите: /values'
+<количество перводимой валюты>\n- Показать список доступных валют введите: /values \
+\n- Напомнить что я могу через команду /help'
     bot.reply_to(message, text)
+
+@bot.message_handler(commands=['help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать конвертацию, введите команду боту в следующем формате: \n<имя валюты> <в какую валюту перевести> <количество переводимой валюты>\nЧтобы увидеть список всех доступных валют, введите команду\n/values'
+    bot.reply_to(message, text)
+
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
@@ -24,7 +31,7 @@ def get_price(message: telebot.types.Message):
         values = message.text.split(' ')
 
         if len(values) != 3:
-            raise APIException('Число параметров не соответствует запрашиваемому.')
+            raise APIException('Число параметров не соответствует формату.\n Введите команду или 3 параметра')
 
         quote, base, amount = values
         total_base = CryptoConverter.get_price(quote, base, amount)
@@ -33,7 +40,7 @@ def get_price(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'Цена {amount} {quote} в {base} = {total_base}'
         bot.send_message(message.chat.id, text)
 
 bot.polling()
